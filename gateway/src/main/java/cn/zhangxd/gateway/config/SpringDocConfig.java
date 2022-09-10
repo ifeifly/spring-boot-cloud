f4b0ad28362928c938be66f41b3f8abfbd5bdcd4
puhaiyang <761396462@qq.com>
2022-09-10 21:12:52 +0800

    add spring doc

--- gateway/src/main/java/cn/zhangxd/gateway/config/SpringDocConfig.java
+++ gateway/src/main/java/cn/zhangxd/gateway/config/SpringDocConfig.java
@@ -21,4 +21,28 @@ public class SpringDocConfig {
                         .description("天下武功，无坚不破，唯快不破")
                         .url("https://blog.csdn.net/puhaiyang"));
     }
+
+
+    /**
+     * springdoc根据请求url自动分组<br>
+     * 参考：<link>https://github.com/springdoc/springdoc-openapi-demos/tree/master/springdoc-openapi-microservices</link>
+     */
+//    @Bean
+//    @Lazy(false)
+//    public List<GroupedOpenApi> apis(SwaggerUiConfigParameters swaggerUiConfigParameters, RouteDefinitionLocator locator) {
+//        List<GroupedOpenApi> groups = new ArrayList<>();
+//        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+//        //遍历定义的路由列表
+//        for (RouteDefinition definition : definitions) {
+//            System.out.println("id: " + definition.getId()+ "  "+definition.getUri().toString());
+//        }
+//        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+//            //获取出名称部分
+//            String name = routeDefinition.getId().replaceAll("-service", "");
+//            //进行自动分组
+//            swaggerUiConfigParameters.addGroup(name);
+//            GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+//        });
+//        return groups;
+//    }
 }
