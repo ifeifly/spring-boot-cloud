fb4ef7c127461c75a3d797be401f31866aad8fc3
puhaiyang <761396462@qq.com>
2022-09-10 19:07:06 +0800

    add spring doc

--- gateway/src/main/java/cn/zhangxd/gateway/GatewayApplication.java
+++ gateway/src/main/java/cn/zhangxd/gateway/GatewayApplication.java
@@ -1,13 +1,9 @@
 package cn.zhangxd.gateway;
 
 
-import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,9 +13,5 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    MeterRegistryCustomizer<MeterRegistry> configurer(
-            @Value("${spring.application.name}") String applicationName) {
-        return (registry) -> registry.config().commonTags("application", applicationName);
-    }
+
 }
---
+++ gateway/src/main/java/cn/zhangxd/gateway/config/SpringDocConfig.java
@@ -0,0 +1,24 @@
+package cn.zhangxd.gateway.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("GATEWAY API")
+                        .description("网关")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("https://github.com/puhaiyang")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("天下武功，无坚不破，唯快不破")
+                        .url("https://blog.csdn.net/puhaiyang"));
+    }
+}
